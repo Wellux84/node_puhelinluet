f8358ea1ca6f30ec45058bcf084fdb3073c5e7f6
Wellux84 <161526426+Wellux84@users.noreply.github.com>
2024-09-13 03:08:13 +0300

    osa c

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static('dist'))
 const cors = require('cors')
 const Person = require('./models/person')
 app.use(cors())
-
+app.use(requestLogger)
 
 
 morgan.token('body', req => {
@@ -15,29 +15,6 @@ morgan.token('body', req => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-  let persons = [
-    { 
-      "name": "Arto Hellas", 
-      "number": "040-123456",
-      "id": "1"
-    },
-    { 
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523",
-      "id": "2"
-    },
-    { 
-      "name": "Dan Abramov", 
-      "number": "12-43-234345",
-      "id": "3"
-    },
-    { 
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "id": "4"
-    }
-  ]
   
   let text = `Phonebook has info for ${persons.length} people`
   let dates =  new Date()
@@ -52,7 +29,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
     })
   })
 
-  app.get('/api/persons/:id', (request, response) => {
+  app.get('/api/persons/:id', (request, response, next) => {
       Person.findById(request.params.id).then(person => {
         if (person) {
           response.json(person)
@@ -60,16 +37,18 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
           response.status(404).end()
         }
     })
+    .catch(error => next(error))
   })
         
 
 
-  app.delete('/api/persons/:id', (request, response) => {
-      const id = request.params.id
-      persons = persons.filter(person => person.id !== id)
-      
-      response.status(204).end()
-    })
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
 
   const generateId = () => {
       const Id = Math.floor(Math.random() * 9000000)
@@ -110,6 +89,25 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
   })
 })
   
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
